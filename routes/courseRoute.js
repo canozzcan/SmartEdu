2faf2c85977dcf166e37d67dd72a25b8da3a133d
canozzcan <canozcanstu@gmail.com>
2022-06-03 23:29:06 +0300

    List Courses - Single Course Page & Slugify

--- routes/courseRoute.js
+++ routes/courseRoute.js
@@ -4,7 +4,8 @@ const courseController = require('../controllers/courseController');
 const router = express.Router();
 
 router.route('/').post(courseController.createCourse);   // localhost:3000/courses
-
+router.route('/').get(courseController.getAllCourses);
+router.route('/:slug').get(courseController.getCourse);   // localhost:3000/courses/:id - localhost:3000/courses/:slug
 
 
 
