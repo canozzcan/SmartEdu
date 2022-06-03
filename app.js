7ba91c30e6cd0635dc10346bd70b94822b871885
canozzcan <canozcanstu@gmail.com>
2022-06-03 19:31:21 +0300

    Create Course Model

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const pageRoute = require('./routes/pageRoute');
+const courseRoute = require('./routes/courseRoute');
 
 const app = express();
+
+// Connect DB
+mongoose.connect('mongodb://localhost/smartedu-db').then(() => {
+    console.log('DB Connected Succesfully');
+});
+
 
 //Template Engine
 app.set("view engine", "ejs");
 
 //Middlewares
 app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 //Routes 
 app.use('/', pageRoute);
+app.use('/courses', courseRoute);
 
 
 const port = 3000;
