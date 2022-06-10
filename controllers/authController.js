8e1b3e057bf1f0b7159cfd6a5a9cd38d8c737978
canozzcan <canozcanstu@gmail.com>
2022-06-10 18:31:24 +0300

    Url Bugfixing Using Custom Middlewares

--- controllers/authController.js
+++ controllers/authController.js
@@ -27,7 +27,7 @@ exports.loginUser = async (req, res) => {
           if (same) {
             // User Session
             req.session.userID = user._id;
-            res.status(200).redirect('/');
+            res.status(200).redirect('/users/dashboard');
           }
         })
       }
@@ -43,4 +43,12 @@ exports.logoutUser = (req, res) => {
   req.session.destroy(() => {
     res.redirect('/');
   })
+}
+
+exports.getDashboardPage = async (req, res) => {
+  const user = await User.findOne({_id: req.session.userID});
+  res.status(200).render('dashboard', {
+      page_name: "dashboard",
+      user
+  });
 }
