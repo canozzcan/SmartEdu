afbf4d709059a976fe7fd9115fb4941bafd3eecc
canozzcan <canozcanstu@gmail.com>
2022-06-11 20:48:27 +0300

    Course - Teacher & Course - Student Relations

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,7 @@
 const User = require('../models/User');
 const Category = require('../models/Category');
 const bcrypt = require('bcrypt');
+const Course = require('../models/Course');
 
 exports.createUser = async (req, res) => {
   try {
@@ -21,12 +22,12 @@ exports.loginUser = async (req, res) => {
 
     const user = await User.findOne({ email: email });
     if (user) {
-      bcrypt.compare(password, user.password, (err, same) => {
-        if (same) {
+      bcrypt.compare(password, user.password, (err) => {
+        
           // User Session
           req.session.userID = user._id;
           res.status(200).redirect('/users/dashboard');
-        }
+        
       })
     }
   } catch (error) {
@@ -44,11 +45,13 @@ exports.logoutUser = (req, res) => {
 }
 
 exports.getDashboardPage = async (req, res) => {
-  const user = await User.findOne({ _id: req.session.userID });
+  const user = await User.findOne({ _id: req.session.userID }).populate('courses');
   const categories = await Category.find();
+  const courses = await Course.find({ user: req.session.userID });
   res.status(200).render('dashboard', {
     page_name: "dashboard",
     user,
-    categories
+    categories,
+    courses
   });
 }
